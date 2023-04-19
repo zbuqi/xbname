ae878485b9afa5ef4b235cb3553340346b6862a6
zbuqi <517851416@qq.com>
2023-04-20 00:32:02 +0800

    修改 element 自定义组件

--- resources/admin/src/views/push/index.vue
+++ resources/admin/src/views/push/index.vue
@@ -1,20 +1,60 @@
 <template>
     <div class="app-container">
-        
+        <el-form class="push-names-wrap" ref="form" :model="form">
+          <el-form-item>
+            <el-input class="push-names" type="textarea" v-model="desc"></el-input>
+            <div class="push-names-num">
+              <el-button type="primary" @click="onSubmit">立即上传</el-button>
+              <div class="ym-num"><p>待提交：<b>{{ ymnum }}</b> 条域名</p></div>
+            </div>
+          </el-form-item>
+        </el-form>
     </div>
 </template>
 
 <script>
 export default{
-    data(){
-
+    data() {
+      return {
+        desc: '',
+        ymnum:''
+      }
     },
-    methods:{
-
-    }
+    updated() {
+      return this.ymnum = this.desc;
+    },
+    methods: {
+      onSubmit() {
+        this.$message('submit!')
+      }
+    },
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+.push-names-wrap{
+  margin:50px;
+}
+.push-names{
+  width:50%;
+  padding:0 10px;
+  float:left;
+}
+.push-names .el-textarea__inner{
+  min-height:180px !important;
+  padding:15px;
+}
+.push-names-num{
+  width:50%;
+  float:left;
+  padding:0 10px;
+}
+.push-names-num .ym-num{
+  font-size:14px;
+  color:#333;
+}
+.push-names-num .ym-num b{
+  font-size: 18px;
+  color: #ff7800;
+}
 </style>
