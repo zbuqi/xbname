a862f0efd2f8a4a0a5bb118b6d4f568de7cb2c54
zbuqi <517851416@qq.com>
2023-05-15 00:32:08 +0800

    gengx

--- app/Http/Controllers/TmpNamesController.php
+++ app/Http/Controllers/TmpNamesController.php
@@ -11,30 +11,21 @@ use App\Http\Middleware\BqFunction;
 
 class TmpNamesController extends Controller
 {
-    public function update(){
-        $names = TmpNames::where('is_beian', false)->where('query_num', 0)->where('id', '<', 50)->get();
-        foreach($names as $item){
-            ProcessPodcast::dispatch($item->name);
-        }
+    public function update()
+    {
+        $names = TmpNames::where('is_beian', false)->where('query_num', 0)->first();
 
-
-        /*
-        $domain = base64_encode($name->name);
+        $domain = base64_encode($names->name);
         $app_id = 'kbigoqlchunrijvq';
         $app_secret = 'a3U1SzA1czNXdTF5cENJOVV3WDZDdz09';
         $params = '?domain=' . $domain . '&app_id=' . $app_id . '&app_secret=' . $app_secret;
         $url = 'https://www.mxnzp.com/api/beian/search' . $params;
         $data = BqFunction::bq_curl_post($url);
-        #$data = json_decode($data);
-        */
-
-        /*
-        if($data['code'] == 0){
-            $content = TmpNames::where('name', $name->name)->update(['query_num' => $name->query_nunm+1]);
+        $data = json_decode($data, true);
+        if ($data['code'] == 0) {
+            print_r($data);
+            #$content = TmpNames::where('name', $name)->update(['query_num' => $name->query_nunm+1]);
         }
-        */
-
-        //return $data;
     }
 
 }
--- app/Jobs/ProcessPodcast.php
+++ app/Jobs/ProcessPodcast.php
@@ -9,6 +9,9 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
+use App\Models\TmpNames;
+use App\Http\Middleware\BqFunction;
+
 class ProcessPodcast implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
@@ -17,7 +20,7 @@ class ProcessPodcast implements ShouldQueue
     /*
     * 最大尝试次数
     */
-    public $tries = 5;
+    public $tries = 1;
 
     /*
     * 任务执行的最大秒数。
@@ -31,7 +34,13 @@ class ProcessPodcast implements ShouldQueue
      */
     public function __construct($name)
     {
-        $this->name = $name;
+        $domain = base64_encode($name);
+        $app_id = 'kbigoqlchunrijvq';
+        $app_secret = 'a3U1SzA1czNXdTF5cENJOVV3WDZDdz09';
+        $params = '?domain=' . $domain . '&app_id=' . $app_id . '&app_secret=' . $app_secret;
+        $url = 'https://www.mxnzp.com/api/beian/search' . $params;
+        $data = BqFunction::bq_curl_post($url);
+        $this->name = $data;
     }
 
     /**
